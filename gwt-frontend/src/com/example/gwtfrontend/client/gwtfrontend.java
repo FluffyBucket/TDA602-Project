8f83a9e7298834c452d82cdfed79d96bb3228203
Daniel Posch <danniwezz@hotmail.com>
2018-05-02 16:28:03 +0200

    added textfield for input in gwt

--- gwt-frontend/src/com/example/gwtfrontend/client/gwtfrontend.java
+++ gwt-frontend/src/com/example/gwtfrontend/client/gwtfrontend.java
@@ -18,22 +18,25 @@ public class gwtfrontend implements EntryPoint {
     public void onModuleLoad() {
         final Button button = new Button("Click me");
         final Label label = new Label();
+        final TextBox tb = new TextBox();
 
         button.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    gwtfrontendService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-                } else {
-                    label.setText("");
-                }
+
+                    gwtfrontendService.App.getInstance().getMessage(tb.getText(), new MyAsyncCallback(label));
+
+                    label.setText(tb.getText());
+                
             }
         });
+
 
         // Assume that the host HTML has elements defined whose
         // IDs are "slot1", "slot2".  In a real app, you probably would not want
         // to hard-code IDs.  Instead, you could, for example, search for all
         // elements with a particular CSS class and replace them with widgets.
         //
+        RootPanel.get().add(tb);
         RootPanel.get("slot1").add(button);
         RootPanel.get("slot2").add(label);
     }
